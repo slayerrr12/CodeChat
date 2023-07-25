9687c71034a2a5283abbc39e44d423d47f0d62d4
slayerrr12 <eyeamarinsharma@gmail.com>
2023-07-25 17:51:38 +0530

    added a thank and a contact us view and also added the form strucutre n the the contact view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "StreamScript : a platform for collaborative coding " });
+  res.render("index", {
+    title: "StreamScript : a platform for collaborative coding ",
+  });
 });
 
 router.get("/about", function name(req, res, next) {
@@ -12,11 +14,16 @@ router.get("/about", function name(req, res, next) {
   });
 });
 
-router.get("/contact",function name(req, res, next) {
-  console.log("hitting this contact middleware")
+router.route("/contact");
+get(function name(req, res, next) {
+  console.log("hitting this contact middleware");
   res.render("contact", {
     title: "StreamScript : a platform for collaborative coding ",
   });
-})
+}).post(function name(req, res, next) {
+  res.render('thank' , {
+    title : 'StreamScript : a platform for collaborative coding '
+  })
+});
 
 module.exports = router;
