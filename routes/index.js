0872f0112450a185bbb8d0873c7f50963d52df86
slayerrr12 <eyeamarinsharma@gmail.com>
2023-07-25 22:36:18 +0530

    validation fix attempt 1

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,36 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", {
-    title: "StreamScript : a platform for collaborative coding ",
-  });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Code4Share - a platform for sharing code.' });
 });
 
-router.get("/about", function name(req, res, next) {
-  res.render("about", {
-    title: "StreamScript : a platform for collaborative coding ",
-  });
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Code4Share - a platform for sharing code.'});
 });
 
-router.route("/contact").
-get(function name(req, res, next) {
-  console.log("hitting this contact middleware");
-  res.render("contact", {
-    title: "StreamScript : a platform for collaborative coding ",
-  });
-}).post(function name(req, res, next) {
-  res.render('thank' , {
-    title : 'StreamScript : a platform for collaborative coding '
+router.route('/contact')
+  .get(function(req, res, next) {
+    res.render('contact', { title: 'Code4Share - a platform for sharing code.'});
   })
-});
+  .post(function(req, res, next) {
+    req.checkBody('name', 'Empty name').notEmpty();
+    req.checkBody('email', 'Invalid email').isEmail();
+    req.checkBody('message', 'Empty message').notEmpty();
+    var errors = req.validationErrors();
+
+    if(errors) {
+      res.render('contact', {
+        title: 'Code4Share - a platform for sharing code.',
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message,
+        errorMessages: errors
+      });
+    } else {
+      res.render('thank', { title: 'Code4Share - a platform for sharing code.'});
+    }
+  });
 
 module.exports = router;
