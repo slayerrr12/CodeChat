f8e50857e3f8482ef66231d9c78107d707d546ba
slayerrr12 <eyeamarinsharma@gmail.com>
2023-07-25 16:59:33 +0530

    added a  middleware to handle about endpoint inside the index file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "StreamScript : a platform for collaborative coding " });
+});
+
+router.get("/about", function name(req, res, next) {
+  res.render("about", {
+    title: "StreamScript : a platform for collaborative coding ",
+  });
 });
 
 module.exports = router;
