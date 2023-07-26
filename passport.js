8e4ccb56d40ecec8ed19335a8546cf440a962d5f
slayerrr12 <eyeamarinsharma@gmail.com>
2023-07-27 03:02:54 +0530

    refactoring variables

--- passport.js
+++ passport.js
@@ -3,40 +3,40 @@ const user = require("./models/user");
 const LocalStrategy = require("passport-local").Strategy;
 
 passport.serializeUser(function (user, done) {
-  done(null, user._id);
+    done(null, user._id);
 });
 
 passport.deserializeUser(function (id, done) {
-  user.findOne({ _id: id }, function (err, user) {
-    done(err, user);
-  });
+    user.findOne({ _id: id }, function (err, user) {
+        done(err, user);
+    });
 });
 
 passport.use(
-  new LocalStrategy(
-    {
-      usernameField: "email", // by default it is "username" but we want to use
-    },
-    function (username, password, done) {
-      UserActivation.findOne(
+    new LocalStrategy(
         {
-          email: username,
+            usernameField: "email", // by default it is "username" but we want to use
         },
-        function (err, done) {
-          if (err) return done(err);
-          if (!user) {
-            return done(null, false, {
-              message: "incorrect username or password",
-            });
-          }
-          if (!user.validPassword(password)) {
-            return done(null, false, {
-              message: "Incorrect ",
-            });
-          }
-          return done(null , user);
+        function (username, password, done) {
+            user.findOne(
+                {
+                    email: username,
+                },
+                function (err, done) {
+                    if (err) return done(err);
+                    if (!user) {
+                        return done(null, false, {
+                            message: "incorrect username or password",
+                        });
+                    }
+                    if (!user.validPassword(password)) {
+                        return done(null, false, {
+                            message: "Incorrect ",
+                        });
+                    }
+                    return done(null, user);
+                }
+            );
         }
-      );
-    }
-  )
+    )
 );
