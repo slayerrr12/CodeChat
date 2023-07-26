d7d067e0f505aea508729baeae75439a997dfeda
slayerrr12 <eyeamarinsharma@gmail.com>
2023-07-26 14:44:34 +0530

    hope this works

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+var config = require('../config');
+var transporter = nodemailer.createTransport(config.mailer)
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Code4Share - a platform for sharing code.' });
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'Code4Share - a platform for sharing code.'});
+router.get('/about', function (req, res, next) {
+  res.render('about', { title: 'Code4Share - a platform for sharing code.' });
 });
 
 router.route('/contact')
-  .get(function(req, res, next) {
-    res.render('contact', { title: 'Code4Share - a platform for sharing code.'});
+  .get(function (req, res, next) {
+    res.render('contact', { title: 'Code4Share - a platform for sharing code.' });
   })
-  .post(function(req, res, next) {
+  .post(function (req, res, next) {
     req.checkBody('name', 'Empty name').notEmpty();
     req.checkBody('email', 'Invalid email').isEmail();
     req.checkBody('message', 'Empty message').notEmpty();
     var errors = req.validationErrors();
 
-    if(errors) {
+    if (errors) {
       res.render('contact', {
         title: 'Code4Share - a platform for sharing code.',
         name: req.body.name,
@@ -29,7 +32,21 @@ router.route('/contact')
         errorMessages: errors
       });
     } else {
-      res.render('thank', { title: 'Code4Share - a platform for sharing code.'});
+      const mailoptions = {
+        from: 'Code4Share <no-reply@code4share.com>',
+        to: "eyeamarinsharma@gmail.com",
+        subject: 'You got a new message from the visitor',
+        text: req.body.message
+      }
+      transporter.sendMail(mailoptions, function (error, info) {
+        if (error) {
+          return console.log(error)
+
+        }
+        res.render('thank', { title: 'Code4Share - a platform for sharing code.' });
+      })
+
+
     }
   });
 
