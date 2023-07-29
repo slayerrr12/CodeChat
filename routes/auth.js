7cd0714e9f08371d819455994034d03042932dd5
slayerrr12 <eyeamarinsharma@gmail.com>
2023-07-29 23:05:34 +0530

    refactoring auth code with updated validator package

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const passport = require('../passport');
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator')
 
 router.get('/login', function (req, res, next) {
     res.render('login', {
@@ -9,42 +10,50 @@ router.get('/login', function (req, res, next) {
 })
 
 
-router.route('/register').post(function (req, res, next) {
+router.route('/register').post(
+    [
+        body('name').notEmpty().withMessage('Empty Name'),
+        body('email').isEmail().withMessage('Invalid Email'),
+        body('password').notEmpty().withMessage('Empty Password'),
+        body('confirmPassword')
+            .notEmpty().withMessage('Empty Confirmation Password')
+            .custom((value, { req }) => value === req.body.password)
+            .withMessage('Passwords do not match')
+    ]
+    ,
+    function (req, res, next) {
 
-    req.checkBody('name', 'Empty Name').notEmpty();
-    req.checkBody('email', 'Invalid Email').isEmail();
-    req.checkBody('password', 'Empty Name').notEmpty();
-    req.checkBody('password', 'Password do not match').equals(req.body.confirmPassword).notEmpty();
 
-    const errors = req.validationErrors();
-    if (errors) {
-        req.render('register', {
-            name: req.body.name,
-            email: req.body.email,
-            errorMessages: errors
+
+        const errors = validationResult(req);
+        if (errors) {
+            req.render('register', {
+                name: req.body.name,
+                email: req.body.email,
+                errorMessages: errors
+            })
+        }
+        else {
+            let user = new User();
+            user.name = req.body.name
+            user.email = req.body.email;
+            user.setPassword(req.body.passport)
+            user.save(function (err) {
+                if (err) {
+                    res.render('register', {
+                        errorMessages: err
+                    })
+
+                }
+                else {
+                    res.redirect('/login')
+                }
+            })
+
+        }
+
+    }).get('/register', function (req, res, next) {
+        res.render('register', {
+            title: 'register'
         })
-    }
-    else{
-        let user = new User();
-        user.name = req.body.name
-        user.email = req.body.email;
-        user.setPassword(req.body.passport)
-        user.save(function(err){
-            if (err) {
-                res.render('register',{
-                    errorMessages:err
-                })
-                
-            }
-            else{
-                res.redirect('/login')
-            }
-        })
-
-    }
-
-}).get('/register', function (req, res, next) {
-    res.render('register', {
-        title: 'register'
     })
-})
