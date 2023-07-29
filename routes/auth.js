32d96be33d157b41e44265ad9880cba760010b73
slayerrr12 <eyeamarinsharma@gmail.com>
2023-07-29 22:39:49 +0530

    controllers for the registor logic

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,50 @@
-const config = require('../config');
+const passport = require('../passport');
 const express = require('express');
 const router = express.Router();
 
-router.get('/login', function (req , res ,next) {
+router.get('/login', function (req, res, next) {
     res.render('login', {
-      title : 'login'
+        title: 'login'
     })
-  })
-  router.get('/register', function (req , res ,next) {
+})
+
+
+router.route('/register').post(function (req, res, next) {
+
+    req.checkBody('name', 'Empty Name').notEmpty();
+    req.checkBody('email', 'Invalid Email').isEmail();
+    req.checkBody('password', 'Empty Name').notEmpty();
+    req.checkBody('password', 'Password do not match').equals(req.body.confirmPassword).notEmpty();
+
+    const errors = req.validationErrors();
+    if (errors) {
+        req.render('register', {
+            name: req.body.name,
+            email: req.body.email,
+            errorMessages: errors
+        })
+    }
+    else{
+        let user = new User();
+        user.name = req.body.name
+        user.email = req.body.email;
+        user.setPassword(req.body.passport)
+        user.save(function(err){
+            if (err) {
+                res.render('register',{
+                    errorMessages:err
+                })
+                
+            }
+            else{
+                res.redirect('/login')
+            }
+        })
+
+    }
+
+}).get('/register', function (req, res, next) {
     res.render('register', {
-      title : 'register'
+        title: 'register'
     })
-  })
+})
