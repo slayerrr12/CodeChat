af900fa8bbdec9e287fc8ad5544dd6ff6b6bbe80
slayerrr12 <eyeamarinsharma@gmail.com>
2023-08-02 02:16:07 +0530

    index view updated

--- app.js
+++ app.js
@@ -6,15 +6,13 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const indexRoutes = require("./routes/index");
 const authRoutes = require("./routes/auth");
+const taskRoutes = require("./routes/task");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const config = require("./config");
 const session = require("express-session");
 
-
 require("./passport");
-
-
 
 mongoose
   .connect(config.dbConnectSting, {
@@ -27,6 +25,7 @@ mongoose
   });
 
 global.user = require("./models/user");
+global.Task = require('./models/task');
 
 const app = express();
 
@@ -60,6 +59,7 @@ app.use(function (req, res, next) {
 });
 app.use("/", indexRoutes);
 app.use("/", authRoutes);
+app.use('/', taskRoutes)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
