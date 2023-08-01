189c5beb78588aadefc9e36018631b976f0d272a
slayerrr12 <eyeamarinsharma@gmail.com>
2023-08-02 01:25:23 +0530

    writing the taskschema

--- models/task.js
+++ models/task.js
@@ -1,1 +1,9 @@
  const mongoose = require('mongoose');
+const { schema } = require('./user');
+ const taskSchema = new mongoose.Schema ([
+
+ ]);
+
+
+
+ module.exports = mongoose.model('Task', taskSchema)
