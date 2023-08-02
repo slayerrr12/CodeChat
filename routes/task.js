2add8409c43cccf9b1434513188467cca5ea407c
slayerrr12 <eyeamarinsharma@gmail.com>
2023-08-02 15:09:18 +0530

    task route controller updation

--- routes/task.js
+++ routes/task.js
@@ -1,38 +1,34 @@
-const express = require("express");
-const task = require("../models/task");
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
+var Task = require('../models/task'); // Make sure to require your Task model
 
-router.get("/createTask", function (req, res) {
-    const newTask = new task();
-
-    newTask.save(function (err, data) {
-        if (!err && data != null) {
-            console.log(err);
-            res.render("error");
-        } else {
-            res.redirect("/task/" + data.id);
-        }
-    });
+router.get('/createTask', async function(req, res) {
+  try {
+    var newTask = new Task();
+    await newTask.save();
+    res.redirect('/task/' + newTask._id);
+  } catch (err) {
+    console.log(err);
+    res.render('error');
+  }
 });
 
-router.get("/task/:id", function (req, res) {
+router.get('/task/:id', async function(req, res) {
+  try {
     if (req.params.id) {
-        task.findOne({ _id: req.params.id }, function (err, data) {
-            if (err) {
-                console.log(err);
-                res.render("error");
-            }
-            if (data) {
-                res.render("task", { data: data });
-            }
-            else {
-                res.render('error');
-            }
-        });
-    }
-    else{
+      var data = await Task.findOne({_id: req.params.id});
+      if (data) {
+        res.render('task', {data: data});
+      } else {
         res.render('error');
+      }
+    } else {
+      res.render('error');
     }
+  } catch (err) {
+    console.log(err);
+    res.render('error');
+  }
 });
 
 module.exports = router;
