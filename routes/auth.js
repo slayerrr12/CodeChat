fef586608861b30c96651cac6aa8b8211f748519
slayerrr12 <eyeamarinsharma@gmail.com>
2023-08-04 01:53:23 +0530

    slight tweak in task.hbs file

--- routes/auth.js
+++ routes/auth.js
@@ -58,9 +58,14 @@ router
         });
     });
 
-router.get("/logout", function (req, res, next) {
-    req.logout();
-    res.redirect("/");
-});
+    router.get("/logout", function (req, res, next) {
+        req.logout(function(err) {
+            if (err) {
+                return next(err);
+            }
+            res.redirect("/");
+        });
+    });
+    
 
 module.exports = router;
