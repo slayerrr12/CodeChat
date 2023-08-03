f05b0c21024f4b7c3f68498906601784b8c500dd
slayerrr12 <eyeamarinsharma@gmail.com>
2023-08-04 02:03:40 +0530

    creating chat rooms

--- socket-server.js
+++ socket-server.js
@@ -1,10 +1,15 @@
 const socketIO = require("socket.io");
 
 module.exports = function (server) {
-  const io = socketIO(server);
-  io.on("connection", function (socket) {
-    socket.on("chatMessage", function (data) {
-      io.emit("chatMessage", data);
+    const io = socketIO(server);
+    io.on("connection", function (socket) {
+        socket.on("joinRoom", function (data) {
+            socket.room = data.room;
+            socket.join(data.room);
+        });
+
+        socket.on("chatMessage", function (data) {
+            io.emit("chatMessage", data);
+        });
     });
-  });
 };
