c885cd446778e9930fe8e18106e282662b44e32f
slayerrr12 <eyeamarinsharma@gmail.com>
2023-08-05 17:16:29 +0530

    adding qs library

--- socket-server.js
+++ socket-server.js
@@ -5,14 +5,17 @@ const socketIO = require('socket.io');
 module.exports = function(server) {
   const io = socketIO(server);
   io.on('connection', function(socket) {
+
     socket.on('joinRoom', function(data) {
+      console.log(data.room)
       socket.room = data.room;
       socket.join(data.room);
     });
 
+
     socket.on('chatMessage', function(data) {
       io.to(socket.room).emit('chatMessage', data);
-    });
+    }); 
 
     socket.on('disconnect', function() {
       socket.leave(socket.room);
