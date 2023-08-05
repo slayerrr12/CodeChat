515aa09e4b3403073887efba0ef9bbe26ef19b33
slayerrr12 <eyeamarinsharma@gmail.com>
2023-08-05 16:48:32 +0530

    updated socket.io package

--- socket-server.js
+++ socket-server.js
@@ -1,18 +1,21 @@
-const socketIO = require("socket.io");
+'use strict';
 
-module.exports = function (server) {
-    const io = socketIO(server);
-    io.on("connection", function (socket) {
-        socket.on("joinRoom", function (data) {
-            socket.room = data.room;
-            socket.join(data.room);
-        });
+const socketIO = require('socket.io');
 
-        socket.on("chatMessage", function (data) {
-            io.to(socket.room).emit('chatMessage', data);
-        });
-        socket.on('disconnect', function () {
-            socket.leave(socket.room)
-        })
+module.exports = function(server) {
+  const io = socketIO(server);
+  io.on('connection', function(socket) {
+    socket.on('joinRoom', function(data) {
+      socket.room = data.room;
+      socket.join(data.room);
     });
-};
+
+    socket.on('chatMessage', function(data) {
+      io.to(socket.room).emit('chatMessage', data);
+    });
+
+    socket.on('disconnect', function() {
+      socket.leave(socket.room);
+    });
+  })
+}
